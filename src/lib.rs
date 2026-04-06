6a1889c63482531199c27dbf09ec981db1a05643
kresnawan <sanjayakresnawan@gmail.com>
2026-04-06 10:23:02 +0700

    Modulate the structs and enums, a little of testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod app;
 mod card;
 mod card_game;
+mod deck;
 mod player;
 mod session;
 mod session_config;
@@ -9,7 +10,13 @@ mod session_manager;
 pub use crate::app::App;
 
 pub use crate::card::Card;
+pub use crate::card::CardIcon;
 pub use crate::card::CardType;
+pub use crate::card::CourtType;
+pub use crate::card::JokerType;
+pub use crate::card::SpotNumber;
+
+pub use crate::deck::Deck;
 
 pub use crate::card_game::CardGame;
 pub use crate::player::Player;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,41 @@
-use remyan::Card;
+use remyan::*;
 
 fn main() {
-    let deck = Card::generate_deck(false);
-    println!("{:#?}", deck);
-    println!("{}", deck.len());
+    let mut instance = App::new();
+    instance
+        .register_new_player(25, String::from("Andres"))
+        .unwrap();
+
+    instance
+        .register_new_player(23, String::from("Sergio"))
+        .unwrap();
+
+    instance
+        .register_new_player(22, String::from("Martin"))
+        .unwrap();
+
+    match instance.create_session(
+        1,
+        21,
+        SessionConfig::new(true, true, false, false, None).unwrap(),
+    ) {
+        Ok(_) => {}
+        Err(err) => {
+            println!("{}", err);
+        }
+    }
+
+    match instance.create_session(
+        1,
+        23,
+        SessionConfig::new(true, true, true, true, None).unwrap(),
+    ) {
+        Ok(_) => {}
+        Err(err) => {
+            println!("{}", err);
+        }
+    }
+
+    println!("{:#?}", instance.players);
+    println!("{:#?}", instance.session_manager);
 }
