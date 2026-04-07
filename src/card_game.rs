cacf4e6df6829f277cfbcb8689334302eb9f4635
kresnawan <sanjayakresnawan@gmail.com>
2026-04-07 11:03:22 +0700

    Minimizing the CardGame's attribute as it is orchestrated by its session

--- src/card_game.rs
+++ src/card_game.rs
@@ -1,7 +1,19 @@
-use crate::Card;
+use crate::{SessionConfig};
 
 #[derive(Debug)]
 pub struct CardGame {
-    pub card_stack: Vec<Card>,
-    pub bookie_id: u32,
+    
+    pub config_used: SessionConfig,
+    pub dealer_id: u32,
+    pub current_turn: u32
+}
+
+impl CardGame {
+    pub fn new(cfg: SessionConfig, dealer_id: u32) -> Self {
+        Self {
+            config_used: cfg, 
+            dealer_id: dealer_id, 
+            current_turn: dealer_id 
+        }
+    }
 }
